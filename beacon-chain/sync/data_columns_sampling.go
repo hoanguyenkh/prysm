393f615c4f8ecd0681447cac324e902a282b9b82
Manu NALEPA <enalepa@offchainlabs.com>
2024-06-13 18:15:31 +0200

    Implement LossyDAS with (arbitrarily) `allowedFailures == 1`.

--- beacon-chain/sync/data_columns_sampling.go
+++ beacon-chain/sync/data_columns_sampling.go
@@ -20,6 +20,10 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// This const will be replaced when IncrementalDAS is implemented
+// https://ethresear.ch/t/lossydas-lossy-incremental-and-diagonal-sampling-for-data-availability/18963#incrementaldas-dynamically-increase-the-sample-size-10
+const allowedFailures = 1
+
 // randomUniformIntegersWithoutReplacement returns a map of `count` random integers, without replacement in the range [0, max[.
 func randomUniformIntegersWithoutReplacement(count uint64, max uint64) map[uint64]bool {
 	result := make(map[uint64]bool, count)
@@ -48,43 +52,48 @@ func sortedListFromMap(m map[uint64]bool) []uint64 {
 }
 
 // sampleDataColumnFromPeer samples data columns from a peer.
-// It returns the missing columns after sampling.
+// It returns:
+// - columns to sample minus the columns the peer should custody, and
+// - columns the peer should custody but we failed to sample from.
 func (s *Service) sampleDataColumnFromPeer(
 	pid peer.ID,
 	columnsToSample map[uint64]bool,
 	requestedRoot [fieldparams.RootLength]byte,
-) (map[uint64]bool, error) {
-	// Define missing columns.
-	missingColumns := make(map[uint64]bool, len(columnsToSample))
-	for index := range columnsToSample {
-		missingColumns[index] = true
-	}
-
+) (map[uint64]bool, map[uint64]bool, error) {
 	// Retrieve the custody count of the peer.
 	peerCustodiedSubnetCount := s.cfg.p2p.CustodyCountFromRemotePeer(pid)
 
 	// Extract the node ID from the peer ID.
 	nodeID, err := p2p.ConvertPeerIDToNodeID(pid)
 	if err != nil {
-		return nil, errors.Wrap(err, "extract node ID")
+		return nil, nil, errors.Wrap(err, "extract node ID")
 	}
 
 	// Determine which columns the peer should custody.
 	peerCustodiedColumns, err := peerdas.CustodyColumns(nodeID, peerCustodiedSubnetCount)
 	if err != nil {
-		return nil, errors.Wrap(err, "custody columns")
+		return nil, nil, errors.Wrap(err, "custody columns")
 	}
 
 	peerCustodiedColumnsList := sortedListFromMap(peerCustodiedColumns)
 
 	// Compute the intersection of the columns to sample and the columns the peer should custody.
+	// Compute the columns still to be sampled after the peer has been queried.
 	peerRequestedColumns := make(map[uint64]bool, len(columnsToSample))
+	remainingColumns := make(map[uint64]bool, len(columnsToSample))
+
 	for column := range columnsToSample {
+		// If the peer should custody the column, we add it to peer requested columns.
 		if peerCustodiedColumns[column] {
 			peerRequestedColumns[column] = true
+			continue
 		}
+
+		// Else, this column should be sampled from another peer.
+		remainingColumns[column] = true
 	}
 
+	remainingColumnsList := sortedListFromMap(remainingColumns)
 	peerRequestedColumnsList := sortedListFromMap(peerRequestedColumns)
 
 	// Get the data column identifiers to sample from this peer.
@@ -103,13 +112,13 @@ func (s *Service) sampleDataColumnFromPeer(
 			"custodiedColumns": peerCustodiedColumnsList,
 			"requestedColumns": peerRequestedColumnsList,
 		}).Debug("Peer does not custody any of the requested columns")
-		return columnsToSample, nil
+		return columnsToSample, nil, nil
 	}
 
 	// Sample data columns.
 	roDataColumns, err := SendDataColumnSidecarByRoot(s.ctx, s.cfg.clock, s.cfg.p2p, pid, s.ctxMap, &dataColumnIdentifiers)
 	if err != nil {
-		return nil, errors.Wrap(err, "send data column sidecar by root")
+		return nil, nil, errors.Wrap(err, "send data column sidecar by root")
 	}
 
 	peerRetrievedColumns := make(map[uint64]bool, len(roDataColumns))
@@ -138,30 +147,42 @@ func (s *Service) sampleDataColumnFromPeer(
 				"peerID":           pid,
 				"retrievedColumn":  retrievedColumn,
 				"requestedColumns": peerRequestedColumnsList,
-			}).Warning("Retrieved column is was not requested")
+			}).Warning("Retrieved column was not requested")
 		}
+	}
 
-		delete(missingColumns, retrievedColumn)
+	// Get all the requested but not retrieved columns.
+	peerMissingColumns := make(map[uint64]bool, min(0, len(peerRequestedColumns)-len(peerRetrievedColumns)))
+	for column := range peerRequestedColumns {
+		if !peerRetrievedColumns[column] {
+			peerMissingColumns[column] = true
+		}
 	}
 
 	peerRetrievedColumnsList := sortedListFromMap(peerRetrievedColumns)
-	remainingMissingColumnsList := sortedListFromMap(missingColumns)
+	peerMissingColumnsList := sortedListFromMap(peerMissingColumns)
 
 	log.WithFields(logrus.Fields{
-		"peerID":                  pid,
-		"custodiedColumns":        peerCustodiedColumnsList,
-		"requestedColumns":        peerRequestedColumnsList,
-		"retrievedColumns":        peerRetrievedColumnsList,
-		"remainingMissingColumns": remainingMissingColumnsList,
+		"peerID":               pid,
+		"peerCustodiedColumns": peerCustodiedColumnsList,
+		"peerRequestedColumns": peerRequestedColumnsList,
+		"peerRetrievedColumns": peerRetrievedColumnsList,
+		"peerMissingColumns":   peerMissingColumnsList,
+		"remainingColumns":     remainingColumnsList,
 	}).Debug("Peer data column sampling summary")
 
-	return missingColumns, nil
+	return remainingColumns, peerMissingColumns, nil
 }
 
 // sampleDataColumns samples data columns from active peers.
-func (s *Service) sampleDataColumns(requestedRoot [fieldparams.RootLength]byte, samplesCount uint64) error {
+func (s *Service) sampleDataColumns(
+	requestedRoot [fieldparams.RootLength]byte,
+	samplesCount uint64,
+	allowedFailuresCount uint64,
+) error {
 	// Determine `samplesCount` random column indexes.
 	requestedColumns := randomUniformIntegersWithoutReplacement(samplesCount, params.BeaconConfig().NumberOfColumns)
+	requestedColumnsList := sortedListFromMap(requestedColumns)
 
 	missingColumns := make(map[uint64]bool, len(requestedColumns))
 	for index := range requestedColumns {
@@ -175,31 +196,58 @@ func (s *Service) sampleDataColumns(requestedRoot [fieldparams.RootLength]byte, 
 
 	// Sampling is done sequentially peer by peer.
 	// TODO: Add parallelism if (probably) needed.
+	failures := make(map[uint64]bool)
+
 	for _, pid := range activePeers {
-		// Early exit if all needed columns are already sampled. (This is the happy path.)
-		if len(missingColumns) == 0 {
+		// Early exit the missing columns count is less than the allowed failures count.
+		// This is the happy path.
+		missingColumnsCount := uint64(len(missingColumns))
+		if missingColumnsCount <= allowedFailuresCount {
 			break
 		}
 
+		var peerFailures map[uint64]bool
+
 		// Sample data columns from the peer.
-		missingColumns, err = s.sampleDataColumnFromPeer(pid, missingColumns, requestedRoot)
+		missingColumns, peerFailures, err = s.sampleDataColumnFromPeer(pid, missingColumns, requestedRoot)
 		if err != nil {
 			return errors.Wrap(err, "sample data column from peer")
 		}
+
+		// Add the peer failures to the global failures.
+		for column := range peerFailures {
+			failures[column] = true
+		}
+
+		// Compute the total number of failures.
+		failuresCount := uint64(len(failures))
+
+		if failuresCount > allowedFailuresCount {
+			missingColumnsList := sortedListFromMap(missingColumns)
+
+			log.WithFields(logrus.Fields{
+				"requestedColumns": requestedColumnsList,
+				"missingColumns":   missingColumnsList,
+			}).Warning("Failed to sample some requested columns")
+
+			return nil
+		}
 	}
 
-	requestedColumnsList := sortedListFromMap(requestedColumns)
+	if len(missingColumns) > 0 {
+		// All peers have been queried and some columns are still missing.
+		missingColumnsList := sortedListFromMap(missingColumns)
 
-	if len(missingColumns) == 0 {
-		log.WithField("requestedColumns", requestedColumnsList).Debug("Successfully sampled all requested columns")
+		log.WithFields(logrus.Fields{
+			"requestedColumns": requestedColumnsList,
+			"missingColumns":   missingColumnsList,
+		}).Warning("Failed to sample all requested columns (peers exhausted)")
+
 		return nil
 	}
 
-	missingColumnsList := sortedListFromMap(missingColumns)
-	log.WithFields(logrus.Fields{
-		"requestedColumns": requestedColumnsList,
-		"missingColumns":   missingColumnsList,
-	}).Warning("Failed to sample some requested columns")
+	// This is the happy path.
+	log.WithField("requestedColumns", requestedColumnsList).Debug("Successfully sampled all requested columns")
 
 	return nil
 }
@@ -208,6 +256,16 @@ func (s *Service) dataColumnSampling(ctx context.Context) {
 	// Create a subscription to the state feed.
 	stateChannel := make(chan *feed.Event, 1)
 	stateSub := s.cfg.stateNotifier.StateFeed().Subscribe(stateChannel)
+
+	// Compute the count of data columns to sample.
+	samplesPerSlot := int64(params.BeaconConfig().SamplesPerSlot)
+	dataColumnSamplingCountInt64, err := peerdas.ExtendedSampleCount(samplesPerSlot, allowedFailures)
+	if err != nil {
+		log.WithError(err).Error("Failed to compute data column sampling count")
+		return
+	}
+
+	dataColumnSamplingCount := uint64(dataColumnSamplingCountInt64)
 
 	// Unsubscribe from the state feed when the function returns.
 	defer stateSub.Unsubscribe()
@@ -249,10 +307,8 @@ func (s *Service) dataColumnSampling(ctx context.Context) {
 				continue
 			}
 
-			dataColumnSamplingCount := params.BeaconConfig().SamplesPerSlot
-
 			// Sample data columns.
-			if err := s.sampleDataColumns(data.BlockRoot, dataColumnSamplingCount); err != nil {
+			if err := s.sampleDataColumns(data.BlockRoot, dataColumnSamplingCount, allowedFailures); err != nil {
 				log.WithError(err).Error("Failed to sample data columns")
 			}
 
